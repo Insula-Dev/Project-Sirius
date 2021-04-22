03f00ccbec0a55ef11fad5e87e297b015a012035
Pablo Trett <pablo.trett@gmail.com>
2021-04-22 16:50:21 +0100

    Role functionality added
    - Emoji encoding fixed for unicode characters
    - Role adding and removing functional
    - Proper exceptions added to role adding and removing functionality

--- bot.py
+++ bot.py
@@ -18,10 +18,15 @@ class MyClient(discord.Client):
 
 	async def on_ready(self):
 
-		print(self.user, "is ready.")  # Event log
+		# Event log
+		print(self.user, "is ready.")
+		if self.guilds != []:
+			print(self.user, "is connected to the following guilds:")
+			for guild in self.guilds:
+				print(guild.name + " (ID: " + str(guild.id) + ")")
 
 		# Load the file data into the data variable
-		with open("data.json") as file:
+		with open("data.json", encoding='utf-8') as file:
 			self.data = json.load(file)
 
 		print("------")  # Event log
@@ -61,8 +66,8 @@ class MyClient(discord.Client):
 
 			# Add emojis to the roles message
 			for role in self.data["servers"][str(guild.id)]["roles"]:
-				print(role)
-				await roles_message.add_reaction(discord.PartialEmoji(name=self.data["servers"][str(guild.id)]["roles"][role]["emoji"]))
+				print(role, self.data["servers"][str(guild.id)]["roles"][role]["emoji"])
+				await roles_message.add_reaction(self.data["servers"][str(guild.id)]["roles"][role]["emoji"])
 
 		# Joke functionality: Shut up Arun
 		if message.author.id == 258284765776576512:
@@ -94,9 +99,91 @@ class MyClient(discord.Client):
 			await message.channel.send("Doggie down")
 			await client.close()
 
+	async def on_raw_reaction_add(self, payload: discord.RawReactionActionEvent):
+		"""Gives a role based on a reaction emoji."""
+
+		# Make sure that the message the user is reacting to is the one we care about.
+		if payload.message_id != self.data["servers"][str(payload.guild_id)]["roles message id"]:
+			return
+
+		# Check if we're still in the guild and it's cached.
+		guild = self.get_guild(payload.guild_id)
+		if guild is None:
+			return
+
+		# If the emoji isn't the one we care about then exit as well.
+		role_id = -1
+		for id_counter in self.data["servers"][str(guild.id)]["roles"]:
+			if self.data["servers"][str(guild.id)]["roles"][id_counter]["emoji"] == str(payload.emoji):
+				role_id = int(id_counter)
+				break
+		if role_id == -1:
+			return
+
+		# Make sure the role still exists and is valid.
+		role = guild.get_role(role_id)
+		if role is None:
+			return
+
+		# Finally, add the role.
+		try:
+			await payload.member.add_roles(role)
+			print("Role `" + role.name + "` added to", payload.member.name)  # Event log
+
+		# If we want to do something in case of errors we'd do it here.
+		except discord.HTTPException:
+			print("Exception: discord.HTTPException. Could not add role", role.name, "to", payload.member.name)  # Event log
+
+	async def on_raw_reaction_remove(self, payload: discord.RawReactionActionEvent):
+		"""Removes a role based on a reaction emoji."""
+
+		# Make sure that the message the user is reacting to is the one we care about.
+		if payload.message_id != self.data["servers"][str(payload.guild_id)]["roles message id"]:
+			return
+
+		# Check if we're still in the guild and it's cached.
+		guild = self.get_guild(payload.guild_id)
+		if guild is None:
+			return
+
+		# If the emoji isn't the one we care about then exit as well.
+		role_id = -1
+		for id_counter in self.data["servers"][str(guild.id)]["roles"]:
+			if self.data["servers"][str(guild.id)]["roles"][id_counter]["emoji"] == str(payload.emoji):  # TYPE? AS ABOVE
+				role_id = int(id_counter)
+				break
+		if role_id == -1:
+			return
+
+		# Make sure the role still exists and is valid.
+		role = guild.get_role(role_id)
+		if role is None:
+			return
+
+		# The payload for `on_raw_reaction_remove` does not provide `.member`
+		# so we must get the member ourselves from the payload's `.user_id`.
+
+		# Make sure the member still exists and is valid.
+		member = guild.get_member(payload.user_id)
+		print(payload.user_id, member)
+		if member is None:
+			return
+
+		# Finally, remove the role.
+		try:
+			await member.remove_roles(role)
+			print("Role `" + role.name + "` removed from", member.name)  # Event log
+
+		# If we want to do something in case of errors we'd do it here.
+		except discord.HTTPException:
+			print("Exception: discord.HTTPException. Could not remove role", role.name, "from", payload.member.name)  # Event log
+
 
 # Main body
-client = MyClient()
+intents = discord.Intents.default()
+intents.members = True
+
+client = MyClient(intents=intents)
 client.run(DISCORD_TOKEN)
 
 print("That's all.")
