13a6e79d5021b5cefb821c1a3bf975438857c8fc
Pablo Trett <pablo.trett@gmail.com>
2021-05-04 20:56:51 +0100

    Revert "Merge branch 'main' into Auto-server-installation"
    This reverts commit b73ed74eca3b73661e4ba53b46d76eda851ff8d1, reversing
    changes made to 3c2584ce78944d8462581d3351bf4213f7f9eb24.

--- bot.py
+++ bot.py
@@ -1,13 +1,9 @@
-# Imports
+#Imports
 from random import randint
 import json
 import socket
 import discord
 import re # Needed for propper regex
-
-# Home imports
-from log_handling import *
-
 
 #Variables
 with open("token.txt") as file:
@@ -28,19 +24,20 @@ class MyClient(discord.Client):
 		self.load_data(printed=True)
 
 	def load_data(self,printed=False):
-		logger.info(self.user.name + " is ready")  # Event log
+		if printed:
+			print("---Starting loading process---")
 		if self.guilds != []:
-			logger.info(self.user.name + " is connected to the following guilds:")  # Event log
-			for guild in self.guilds:
-				logger.info(guild.name + " (ID: " + str(guild.id) + ")")  # Event log
+			if printed:
+				print("Connected to the following guilds:")
+				for guild in self.guilds:
+					print("  "+guild.name + " (ID: " + str(guild.id) + ")")
 
 		# Load the file data into the data variable
-		try:
-			with open("data.json", encoding='utf-8') as file:
-				self.data = json.load(file)
-			logger.info("Loaded data.json")  # Event log
-		except:
-			logger.critical("Could not load data.json")  # Event log
+		with open("data.json", encoding='utf-8') as file:
+			self.data = json.load(file)
+
+		if printed:
+			print("---Finished loading process---")  # Event log
 
 	async def on_guild_join(self, guild):
 		print(self.user, "has joined the guild: " + guild.name + " with id:", guild.id)  # Event log
@@ -79,7 +76,7 @@ class MyClient(discord.Client):
 
 	async def on_message(self, message):
 
-		#logger.debug("Message sent by " + message.author.name)  # Event log
+		print("Message sent by", message.author)  # Event log
 
 		# Don't respond to yourself
 		if message.author.id == self.user.id:
@@ -90,8 +87,6 @@ class MyClient(discord.Client):
 
 		# Rules command
 		if message.content == "!rules":
-
-			logger.info("`!rules` called by " + message.author.name)  # Event log
 
 			# Create and send rules embed
 			embed_rules = discord.Embed(title=self.data["servers"][str(guild.id)]["rules"]["title"], description="\n".join(self.data["servers"][str(guild.id)]["rules"]["list"]), color=0x4f7bc5, inline=False)
@@ -107,8 +102,7 @@ class MyClient(discord.Client):
 
 		# Roles command
 		if message.content == "!roles":
-
-			logger.info("`!roles` called by " + message.author.name)  # Event log
+			print("`!roles` called by", message.author)  # Event log
 
 			# Create and send roled embed
 			embed_roles = discord.Embed(title="Role selection", description="React to get a role, unreact to remove it.", color=0x4f7bc5)
@@ -120,6 +114,7 @@ class MyClient(discord.Client):
 
 			# Add emojis to the roles message
 			for role in self.data["servers"][str(guild.id)]["roles"]:
+				print(role, self.data["servers"][str(guild.id)]["roles"][role]["emoji"])
 				await roles_message.add_reaction(self.data["servers"][str(guild.id)]["roles"][role]["emoji"])
 
 			# Read the data from the file
@@ -266,51 +261,44 @@ class MyClient(discord.Client):
 		# Joke functionality, shut up Arun
 		# Core functionality (do not alter)
 		if message.author.id == 258284765776576512:
-
-			logger.info("Arun sighted. Locking on")  # Event log
-
+			print("Arun sighted. Locking on.")  # Event log
 			if randint(1, 10) == 1:
+				print("Arun down.")  # Event log
 				await message.channel.send("shut up arun")
-				logger.info("Arun down.")  # Event log
 			else:
-				logger.info("Mission failed, RTB")  # Event log
+				print("Mission failed, RTB.")  # Event log
 
 		# Joke functionality: Gameboy mention
 		if "gameboy" in message.content.lower():
-			logger.info("`gameboy` mentioned by " + message.author.name)  # Event log
+			print("`gameboy` mentioned by", message.author)  # Event log
 			await message.channel.send("Gameboys are worthless (apart from micro. micro is cool)")
 
 		# Joke functionality: Raspberry mention
 		if "raspberries" in message.content or "raspberry" in message.content:
-			logger.info("`raspberry racers` mentioned by " + message.author.name)  # Event log
-			await message.channel.send("The Raspberry Racers are a team which debuted in the 2018 Winter Marble League. Their 2018 season was seen as the second-best rookie team of the year, behind only the Hazers. In the 2018 off-season, they won the A-Maze-ing Marble Race, making them one of the potential title contenders for the Marble League. They eventually did go on to win Marble League 2019.")
+			print("`raspberry racers` mentioned by", message.author)  # Event log
+			await message.channel.send(
+				"The Raspberry Racers are a team which debuted in the 2018 Winter Marble League. Their 2018 season was seen as the second-best rookie team of the year, behind only the Hazers. In the 2018 off-season, they won the A-Maze-ing Marble Race, making them one of the potential title contenders for the Marble League. They eventually did go on to win Marble League 2019.")
 
 		# Joke functionality: Token command
 		if message.content == "!token":
-			logger.info("`!token` called by " + message.author.name)  # Event log
+			print("`!token` called by", message.author)  # Event log
 			await message.channel.send("IdrOppED ThE TokEN gUYS!!!!")
 
 		# Joke functionality: Summon lizzie command
 		if message.content == "!summon_lizzie":
-			logger.info("`!summon_lizzie` called by " + message.author.name)  # Event log
+			print("`!summon_lizzie` called by", message.author)
 			for x in range(100):
-				await message.channel.send(guild.get_member(692684372247314445).mention)
-
-		# Joke functionality: Teaching bitches how to swim
-		if message.content == "!swim":
-			logger.info("`!swim` called by " + message.author.name)  # Event log
-			await message.channel.send("/play https://youtu.be/uoZgZT4DGSY")
-			await message.channel.send("No swimming lessons today ):")
+				await message.channel.send(guild.get_member(258284765776576512).mention)
 
 		# Locate command
 		if message.content == "!locate":
-			logger.info("`!locate` called by " + message.author.name)  # Event log
+			print("`!locate` called by", message.author)  # Event log
 			hostname = socket.gethostname()
 			await message.channel.send("This instance is being run on **" + hostname + "**, IP address **" + socket.gethostbyname(hostname) + "**.")
 
 		# Kill command
 		if message.content == "!kill":
-			logger.info("`!kill` called by " + message.author.name)  # Event log
+			print("`!kill` called by", message.author)  # Event log
 			await message.channel.send("Doggie down")
 			await client.close()
 
@@ -320,13 +308,11 @@ class MyClient(discord.Client):
 		# Make sure that the message the user is reacting to is the one we care about.
 		if payload.message_id != self.data["servers"][str(payload.guild_id)]["roles message id"]:
 			return
-		print(1)
 
 		# Check if we're still in the guild and it's cached.
 		guild = self.get_guild(payload.guild_id)
 		if guild is None:
 			return
-		print(2)
 
 		# If the emoji isn't the one we care about then exit as well.
 		role_id = -1
@@ -336,23 +322,21 @@ class MyClient(discord.Client):
 				break
 		if role_id == -1:
 			return
-		print(3)
 
 		# Make sure the role still exists and is valid.
 		role = guild.get_role(role_id)
 		if role is None:
 			return
-		print(4)
 
 		# Finally, add the role.
 		try:
 			print("Try to Role `" + role.name + "` added to", payload.member.name)
 			await payload.member.add_roles(role)
-			logger.info("Role `" + role.name + "` added to " + payload.member.name)  # Event log
+			print("Role `" + role.name + "` added to", payload.member.name)  # Event log
 
 		# If we want to do something in case of errors we'd do it here.
 		except discord.HTTPException:
-			logger.error("Exception: discord.HTTPException. Could not add role " + role.name + " to " + payload.member.name)  # Event log
+			print("Exception: discord.HTTPException. Could not add role", role.name, "to", payload.member.name)  # Event log
 
 	async def on_raw_reaction_remove(self, payload: discord.RawReactionActionEvent):
 		"""Removes a role based on a reaction emoji."""
@@ -369,7 +353,7 @@ class MyClient(discord.Client):
 		# If the emoji isn't the one we care about then exit as well.
 		role_id = -1
 		for id_counter in self.data["servers"][str(guild.id)]["roles"]:
-			if self.data["servers"][str(guild.id)]["roles"][id_counter]["emoji"] == str(payload.emoji):
+			if self.data["servers"][str(guild.id)]["roles"][id_counter]["emoji"] == str(payload.emoji):  # TYPE? AS ABOVE
 				role_id = int(id_counter)
 				break
 		if role_id == -1:
@@ -391,11 +375,11 @@ class MyClient(discord.Client):
 		# Finally, remove the role.
 		try:
 			await member.remove_roles(role)
-			logger.info("Role `" + role.name + "` removed from " + member.name)  # Event log
+			print("Role `" + role.name + "` removed from", member.name)  # Event log
 
 		# If we want to do something in case of errors we'd do it here.
 		except discord.HTTPException:
-			logger.error("Exception: discord.HTTPException. Could not remove role " + role.name + " from ", member.name)  # Event log
+			print("Exception: discord.HTTPException. Could not remove role", role.name, "from", payload.member.name)  # Event log
 
 
 # Main body
@@ -405,4 +389,4 @@ intents.members = True
 client = MyClient(intents=intents)
 client.run(DISCORD_TOKEN)
 
-logger.info("That's all\n")  # Event log
+print("That's all.")
