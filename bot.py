0ec1d403ceaf63d968bf2e7ba32db58c03d07e4c
Pablo Trett <pablo.trett@gmail.com>
2021-05-21 13:43:01 +0100

    Made Sirius serious
    ;)

--- bot.py
+++ bot.py
@@ -1,9 +1,7 @@
 # Imports
-from random import randint
 import json
 import socket
 import discord
-import re  # Needed for propper regex
 
 
 # Home imports
@@ -147,193 +145,79 @@ class MyClient(discord.Client):
 			# Write the updated data
 			self.update_data()
 
-		# Add Role (Arun too smart)
-		if message.content.startswith("!add role"):
-			parameter = message.content[len("!add role "):]  # Sets parameter to everything after the command
-			print("Parameter: " + parameter)
-			# Alters rules using the parameters given
+		# Joke functionality
+		if self.data["config"]["jokes"] is True:
 
-			role_name = ""
-			role_id = 0
-			role_emoji = ""
-			parameters = parameter.split(",")  # Splits parameter string into a list
-			for param in parameters:
-				print("Checking: " + param)
-				if param.startswith("name="):
-					print("name")
-					role_name = param[len("name="):]
-					# Find role id for role with name
-					print("Roles are:\n" + str(message.guild.roles))
-					for role in message.guild.roles:
-						print("Role " + role.name)
-						if role.name == role_name:
-							role_id = role.id
-							break
-					if role_id == 0:
-						print("Role \"" + role_name + "\"was not identified")
-				elif param.startswith("emoji="):
-					print("emoji")
-					role_emoji_name = param[len("emoji="):]
-					print(message.guild.emojis)
-					for emoji in message.guild.emojis:
-						print("Emoji " + emoji.name)
-						if emoji.name == role_emoji_name:
-							role_emoji = "<:" + role_emoji_name + ":" + str(emoji.id) + ">"
-							break
-					if role_emoji == "":
-						print("Emoji \"" + role_emoji_name + "\"was not identified")
+			# Shut up Arun
+			if message.author.id == 258284765776576512:
 
-			# Read the data from the file
-			with open("data.json", encoding='utf-8') as data_file:
-				data = json.load(data_file)
-			roles_data = data["servers"][str(message.guild.id)]["roles"]
-			print(roles_data)
+				logger.info("Arun sighted. Locking on")  # Event log
 
-			# Creates new data for server
-			new_roles = {
-				role_id: {
-					"name": role_name,
-					"emoji": role_emoji
-                                    }
-                            }
-			print("Old roles: " + str(roles_data))
-			print("New roles: " + str(new_roles))
+				if randint(1, 10) == 1:
+					await message.channel.send("shut up arun")
+					logger.info("Arun down.")  # Event log
+				else:
+					logger.info("Mission failed, RTB")  # Event log
 
-			# Merges old and new versions, adding new ones and updating existing ones
-			for old_role in roles_data:
-				if old_role == new_roles.keys():
-					roles_data[old_role.key()] = new_roles
-					new_roles.pop(new_roles.keys())
+			# Gameboy mention
+			if "gameboy" in message.content.lower():
+				logger.info("`gameboy` mentioned by " + message.author.name)  # Event log
+				await message.channel.send("Gameboys are worthless (apart from micro. micro is cool)")
 
-			roles_data.update(new_roles)
-			print("Updated roles: " + str(roles_data))
+			# Raspberry mention
+			if "raspberries" in message.content.lower() or "raspberry" in message.content.lower():
+				logger.info("`raspberry racers` mentioned by " + message.author.name)  # Event log
+				await message.channel.send("The Raspberry Racers are a team which debuted in the 2018 Winter Marble League. Their 2018 season was seen as the second-best rookie team of the year, behind only the Hazers. In the 2018 off-season, they won the A-Maze-ing Marble Race, making them one of the potential title contenders for the Marble League. They eventually did go on to win Marble League 2019.")
 
-			data["servers"][str(message.guild.id)]["roles"] = roles_data
-			# Write the updated data to the file
-			with open("data.json", "w", encoding='utf-8') as data_file:
-				json.dump(data, data_file, indent=4)
+			# Pycharm mention
+			if "pycharm" in message.content.lower():
+				logger.info("`pycharm` mentioned by " + message.author.name)  # Event log
+				await message.channel.send("Pycharm enthusiasts vs Sublime Text enjoyers: https://youtu.be/HrkNwjruz5k")
+				await message.channel.send("85 commits in and haha bot print funny is still your sense of humour.")
 
-			self.load_data()
+			# Token command
+			if message.content == "!token":
+				logger.info("`!token` called by " + message.author.name)  # Event log
+				await message.channel.send("IdrOppED ThE TokEN gUYS!!!!")
 
-		# Set Rules (Arun too smart)
-		if message.content.startswith("!set rules"):
-			parameter = message.content[len("!set rules "):]  # Sets parameter to everything after the command
+			# Summon lizzie command
+			if message.content == "!summon_lizzie":
+				logger.info("`!summon_lizzie` called by " + message.author.name)  # Event log
+				for x in range(100):
+					await message.channel.send(guild.get_member(692684372247314445).mention)
 
-			if parameter == "default":  # Resets rules back to default
+			# Summon leo command
+			if message.content == "!summon_leo":
+				logger.info("`!summon_leo` called by " + message.author.name)  # Event log
+				for x in range(100):
+					await message.channel.send(guild.get_member(242790351524462603).mention)
 
-				title = "Title"
-				#description = "Description"
-				image = "none"
-				rules = "Rule 1.Rule 2. Rule 3"
+			# Teaching bitches how to swim
+			if message.content == "!swim":
+				logger.info("`!swim` called by " + message.author.name)  # Event log
+				await message.channel.send("/play https://youtu.be/uoZgZT4DGSY")
+				await message.channel.send("No swimming lessons today ):")
 
-			else:  # Alters rules using the parameters given
+			# Overlay Israel (Warning: DEFCON 1)
+			if message.content == "!israeli_defcon_1":
+				logger.info("`!israeli_defcon_1` called by " + message.author.name)  # Event log
+				await message.channel.send("preemptive apologies...")
+				while True:
+					await message.channel.send(".overlay israel")
 
-				parameters = parameter.split(",")  # Splits parameter string into a list
-				title = self.data["servers"][str(guild.id)]["rules"]["title"]
-				#description = self.data["servers"][str(guild.id)]["rules"]["description"]
-				image = self.data["servers"][str(guild.id)]["rules"]["image link"]
-				rules = self.data["servers"][str(guild.id)]["rules"]["rules list"]
-				for param in parameters:
-					if param.startswith("title="):
-						title = param[len("title="):]
-					#elif param.startswith("description="):
-					#	description = param[len("description="):]
-					elif param.startswith("image="):
-						image = param[len("image="):]
-					elif param.startswith("rules="):
-						rules = re.split("\.\s|\.", param[len(
-							"rules="):])  # Splits the rules after every full stop or, preferably, a full stop followed by a space
+		if self.data["config"]["secure"] is True:
 
-			# Read the data from the file
-			with open("data.json", encoding='utf-8') as data_file:
-				data = json.load(data_file)
-			rules_data = data["servers"][str(message.guild.id)]["rules"]
-			print(rules_data)
+			# Locate command
+			if message.content == "!locate":
+				logger.info("`!locate` called by " + message.author.name)  # Event log
+				hostname = socket.gethostname()
+				await message.channel.send("This instance is being run on **" + hostname + "**, IP address **" + socket.gethostbyname(hostname) + "**.")
 
-			# Creates new data for server
-			new_rules = {
-				"title": title,
-				#"description": description,
-				"image link": image,
-				"rules list": rules
-			}
-			print("Old rules: " + str(rules_data))
-			print("New rules: " + str(new_rules))
-			rules_data = new_rules
-
-			data["servers"][str(message.guild.id)]["rules"] = rules_data
-			# Write the updated data to the file
-			with open("data.json", "w", encoding='utf-8') as data_file:
-				json.dump(data, data_file, indent=4)
-
-		# Joke functionality: Shut up Arun
-		if message.author.id == 258284765776576512:
-
-			logger.info("Arun sighted. Locking on")  # Event log
-
-			if randint(1, 10) == 1:
-				await message.channel.send("shut up arun")
-				logger.info("Arun down.")  # Event log
-			else:
-				logger.info("Mission failed, RTB")  # Event log
-
-		# Joke functionality: Gameboy mention
-		if "gameboy" in message.content.lower():
-			logger.info("`gameboy` mentioned by " + message.author.name)  # Event log
-			await message.channel.send("Gameboys are worthless (apart from micro. micro is cool)")
-
-		# Joke functionality: Raspberry mention
-		if "raspberries" in message.content.lower() or "raspberry" in message.content.lower():
-			logger.info("`raspberry racers` mentioned by " + message.author.name)  # Event log
-			await message.channel.send("The Raspberry Racers are a team which debuted in the 2018 Winter Marble League. Their 2018 season was seen as the second-best rookie team of the year, behind only the Hazers. In the 2018 off-season, they won the A-Maze-ing Marble Race, making them one of the potential title contenders for the Marble League. They eventually did go on to win Marble League 2019.")
-
-		# Joke functionality: Pycharm mention
-		if "pycharm" in message.content.lower():
-			logger.info("`pycharm` mentioned by " + message.author.name)  # Event log
-			await message.channel.send("Pycharm enthusiasts vs Sublime Text enjoyers: https://youtu.be/HrkNwjruz5k")
-			await message.channel.send("85 commits in and haha bot print funny is still your sense of humour.")
-
-		# Joke functionality: Token command
-		if message.content == "!token":
-			logger.info("`!token` called by " + message.author.name)  # Event log
-			await message.channel.send("IdrOppED ThE TokEN gUYS!!!!")
-
-		# Joke functionality: Summon lizzie command
-		if message.content == "!summon_lizzie":
-			logger.info("`!summon_lizzie` called by " + message.author.name)  # Event log
-			for x in range(100):
-				await message.channel.send(guild.get_member(692684372247314445).mention)
-
-		# Joke functionality: Summon leo command
-		if message.content == "!summon_leo":
-			logger.info("`!summon_leo` called by " + message.author.name)  # Event log
-			for x in range(100):
-				await message.channel.send(guild.get_member(242790351524462603).mention)
-
-		# Joke functionality: Teaching bitches how to swim
-		if message.content == "!swim":
-			logger.info("`!swim` called by " + message.author.name)  # Event log
-			await message.channel.send("/play https://youtu.be/uoZgZT4DGSY")
-			await message.channel.send("No swimming lessons today ):")
-
-		# Joke functionality: Overlay Israel (Warning: DEFCON 1)
-		if message.content == "!israeli_defcon_1":
-			logger.info("`!israeli_defcon_1` called by " + message.author.name)  # Event log
-			await message.channel.send("preemptive apologies...")
-			while True:
-				await message.channel.send(".overlay israel")
-
-		# Locate command
-		if message.content == "!locate":
-			logger.info("`!locate` called by " + message.author.name)  # Event log
-			hostname = socket.gethostname()
-			await message.channel.send("This instance is being run on **" + hostname + "**, IP address **" + socket.gethostbyname(hostname) + "**.")
-
-		# Kill command
-		if message.content == "!kill":
-			logger.info("`!kill` called by " + message.author.name)  # Event log
-			await message.channel.send("Doggie down")
-			await client.close()
+			# Kill command
+			if message.content == "!kill":
+				logger.info("`!kill` called by " + message.author.name)  # Event log
+				await message.channel.send("Doggie down")
+				await client.close()
 
 	async def on_member_join(self, member):
 		"""Sends a welcome message directly to the user."""
