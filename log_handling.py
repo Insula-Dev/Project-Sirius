4f4930786a94fbb20e3b7cd4eb5e86cfd0246e52
ArunOsborn <55600843+ArunOsborn@users.noreply.github.com>
2021-12-19 14:44:22 +0000

    Logs in UTF-8 allowing for many new emoji characters

--- log_handling.py
+++ log_handling.py
@@ -10,7 +10,7 @@ LOG_FILE_PATH = "log_file.log"
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
 
-file_handler = logging.FileHandler(LOG_FILE_PATH)
+file_handler = logging.FileHandler(LOG_FILE_PATH,encoding="UTF-8")
 formatter = logging.Formatter("%(asctime)s.%(msecs)03d | %(levelname)s | %(filename)s | %(funcName)s | %(message)s", ("%d/%m/%Y %H:%M:%S"))
 
 file_handler.setFormatter(formatter)
